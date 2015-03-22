d830e60bdead422d3f141757e2cb59e4503d38a6
Erik Van de Water <ev176@georgetown.edu>
2015-03-22 14:27:38 +0000

    Make payments work in test mode

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,5 +16,8 @@ class PaymentsController < ApplicationController
         err = body[:error]
         flash[:error] = "There was an error in processing your card: #{err[:message]}"
     end
+    respond_to do |format|
+      format.html { redirect_to "/purchase", notice: "Purchase was successfully completed.  We'll be in contact shortly!" }
+    end
   end
 end
