ac2a80dba7afd747035fa99c1ce1598fd054096a
Erik Van de Water <ev176@georgetown.edu>
2015-04-08 23:54:30 +0000

    change config/initializers/stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    publishable_key: ENV[ 'STRIPE_PUBLISHABLE_KEY' ],
-    secret_key:      ENV[ 'STRIPE_SECRET_KEY' ]
+    publishable_key: ENV[ 'PUBLISHABLE_KEY' ],
+    secret_key:      ENV[ 'SECRET_KEY' ]
   }
 else
   Rails.configuration.stripe = {
@@ -9,5 +9,3 @@ else
     secret_key:      'sk_test_hkiYUTQzHiCTBfHuUSXpUP7n'
   }
 end
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
