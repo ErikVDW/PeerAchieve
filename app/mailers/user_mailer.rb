5827e7bb123dbc2accef68dbfadcaba171371117
Erik Van de Water <ev176@georgetown.edu>
2015-04-08 13:25:53 +0000

    Edit sendgrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,10 @@ class UserMailer < ApplicationMailer
     
     def contact_form(email, name, message)
         @message = message
+        @name = name
         mail(
             from:       email,
             to:         "erikvdw@comcast.net",
-            subject:    'A new contact form message from #{name}')
+            subject:    'A new contact form message from #{name}' )
     end
 end
