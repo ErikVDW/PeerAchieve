6518b2c544673d7d6d417a549702655ac9fe4924
Erik Van de Water <ev176@georgetown.edu>
2015-04-16 02:47:21 +0000

    Lower validation restriction for message length

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,5 +2,5 @@ class Email < ActiveRecord::Base
   validates_length_of :first_name, :maximum => 25, :minimum => 2
   validates_length_of :last_name, :maximum => 30, :minimum => 2
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates_length_of :message, :maximum => 500, :minimum => 20
+  validates_length_of :message, :maximum => 500, :minimum => 3
 end
